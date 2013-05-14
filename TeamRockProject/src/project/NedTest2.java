53d4789268f46377c5042e1b9703938d71fa13c1
nedleonard <nedleonard@gmail.com>
2013-05-14 10:09:41 +0100

    Ned testing from a second instance of Eclipse

--- TeamRockProject/src/project/NedTest2.java
+++ TeamRockProject/src/project/NedTest2.java
@@ -3,7 +3,7 @@ package project;
 public class NedTest2 {
 
 public static void main(String args[]){
-	System.out.println("Ned testing pushing and fetching");
+	System.out.println("Ned testing pushing and fetching ");
 }
 }
 
